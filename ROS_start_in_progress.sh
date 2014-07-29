518636f18139f99e6fc69a06d874f0760b5fe015
Raphael Nagel <raphael.nagel@posteo.de>
2014-07-29 15:06:02 +0100

    small things

---
+++ ROS_start_in_progress.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#NODE start script, to be replaced with a roslaunch file...
+#Author: Raphael Nagel
+#Date: 29/July/2014
+
+clear
+export ROS_IP=192.168.2.101
+export ROS_MASTER_URI=http://192.168.36.128:11311
